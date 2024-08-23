fc61a5de9761bdd809f93e322cdde2f2b6fdf984
Harry Rigg <riggh@icloud.com>
2024-08-23 21:34:23 +0800

    Improve homepage on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<div class="flex h-full flex-col items-center justify-center gap-2">
-	<span class="text-xl">All your notes. Stored securely in one place.</span>
+<a class="absolute left-1/2 top-6 -translate-x-1/2 text-2xl tracking-tight md:hidden" href="/">
+	NoteLocker
+</a>
+
+<div class="flex h-full flex-col items-center justify-center gap-2 px-5 md:px-0">
+	<span class="text-center text-xl">All your notes. Stored securely in one place.</span>
 	<span>
 		<a href="/register" class="font-bold text-blue-500 underline">Make an account</a> or
 		<a href="/login" class="text-blue-500 underline">login</a>
