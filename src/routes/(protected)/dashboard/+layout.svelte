6b22e17431b95346ca61965f29725d13ea4b57ac
Harry Rigg <riggh@icloud.com>
2024-08-24 12:35:39 +0800

    Improve dashboard layout responsiveness

--- src/routes/(protected)/dashboard/+layout.svelte
+++ src/routes/(protected)/dashboard/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex h-full md:items-center md:justify-center">
-	<Card.Root class="w-full p-6 md:h-[70%] md:w-3/5">
+	<Card.Root class="w-full p-6 md:h-[70%] md:w-4/5 lg:w-3/5">
 		<slot />
 	</Card.Root>
 </div>
